82a41b09449a9ac0b8afe37aecffbb8594a87dca
KalashnikovM <mixa.kalashnikov@gmail.com>
2021-05-11 14:58:42 +0300

    First commit

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,8 @@
+def main():
+    """Run an example code."""
+    # it is ok to have some magical numbers locally
+    print('Welcome to the Brain Games!')
+
+
+if __name__ == '__main__':
+    main()
