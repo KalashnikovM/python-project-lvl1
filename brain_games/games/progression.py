08779c792ddee514a631d5a20d685c3c63245b88
KalashnikovM <mixa.kalashnikov@gmail.com>
2021-06-07 22:37:38 +0300

    fix progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,7 +7,7 @@ def get_question():
     length_of_progression = 15
     start_progression = randint(1, 100)
     answer = randint(0, length_of_progression - 1)
-    resolution = randint(1, 30)
+    resolution = randint(1, 10)
     index_of_num = 0
     question = ''
     while index_of_num < length_of_progression:
