ce0c205d53857ab4459b85e265dad7e0040c0973
KalashnikovM <mixa.kalashnikov@gmail.com>
2021-06-07 21:49:28 +0300

    step 8 test

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,5 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string('May I hawe your name? ')
-    welcome = f'Hello, {name}'
-    print('\n' + welcome)
+    name = prompt.string('May I hawe your name?')
+    print('Hello,', name)
